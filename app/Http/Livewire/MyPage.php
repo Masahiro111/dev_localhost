975a99374d16ec333ae8329f7a05a43a24d4e5c3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-02-20 20:23:44 +0900

    first commit

---
+++ app/Http/Livewire/MyPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MyPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.my-page');
+    }
+}
